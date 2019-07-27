eb424761eb0b9c34433181e147e5076cc6d08b84
Kevin Spies <kevin.spies@gmail.com>
2019-07-27 19:51:17 -0400

    referenceing hot restaurant's api routes. i just added tony on the website and i am seeing his json in the console, i think that is from when i logged req.body, but my post method here was called.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,9 @@
-var friends = [];
+var friendsArray = [
+    {
+        name: "Kevin",
+        photo: "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/ironman-spiderman-homecoming-poster-frontpage-700x354.jpg",
+        scores: [1, 2, 3, 4, 5, 4, 3, 2, 4, 2]
+    }
+];
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,23 @@
 //todo
 var friendData = require("../data/friends.js");
 
+
 module.exports = function (app) {
 
     // Search for Specific Character (or all characters) then provides JSON
     app.get("/api/friends", function (req, res) {
-        console.log("hey i'm trying to access api friends!" + res.json(friendData));
+        console.log("accessing api/friends!" + res.json(friendData));
+        //testing stuff!
+        // var a = +7;
+        // console.log(a);
+        // console.log(req.body);
     });
 
     // If a user sends data to add a new character...
     app.post("/api/friends", function (req, res) {
-        console.log("hey i'm trying to post to api friends!");
+        console.log("posting to api/friends!");
+        console.log(req.body);
+        friendData.push(req.body);
+        res.json(true);
     });
 };
