67367460371d7e422bdbaa38b3c5e4580fd30689
Kevin Spies <kevin.spies@gmail.com>
2019-07-27 12:34:15 -0400

    omg real progress finally... api.friends is showing the empty array that will have objects in it, displaying it as {} i guess because it was called using the .json method so it expects that array to be full of objects, regardless finally rtrhings are pointing where they needd to be and i can start writing real code that is my own woohoooooooooooooooooooooooooo

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 
     // Search for Specific Character (or all characters) then provides JSON
     app.get("/api/friends", function (req, res) {
-        console.log("hey i'm trying to access api friends!");
+        console.log("hey i'm trying to access api friends!" + res.json(friendData));
     });
 
     // If a user sends data to add a new character...
