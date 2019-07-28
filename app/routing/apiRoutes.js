4107c19224e890fb9d3b791f25aeef151840a24e
Kevin Spies <kevin.spies@gmail.com>
2019-07-28 11:21:35 -0400

    new plan, the post from the jquery in the script of my survey page is passing 3 params, i need to add a para to my api route method i believe

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,12 +14,16 @@ module.exports = function (app) {
     });
 
     // If a user sends data to add a new character...
+
+    //waaaiiit i think this function needs to take a third paramater... the friggen userData from the jQuery part of survey script!!! mwahahahahahaha hope
     app.post("/api/friends", function (req, res) {
         console.log("posting to api/friends!");
         console.log("req.body: " + req.body);
         console.log("req: " + req);
         console.log("req.body.friendData: " + req.body.friendData);
+        console.log("req.body.friendData: " + req.body.friendData.friendsArray);
         friendData.push(req.body);
         res.json(true);
+
     });
 };
