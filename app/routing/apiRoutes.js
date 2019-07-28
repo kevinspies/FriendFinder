a1e4b9a1f80533e2228abf72c9f7e73eb5819220
Kevin Spies <kevin.spies@gmail.com>
2019-07-28 12:39:17 -0400

    finaaalllyyy doing compatibilty logic and i think it is going well

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -26,8 +26,32 @@ module.exports = function (app) {
         //hell why would it know, i need to pass this post another parameter
         friendData.push(req.body);
 
-        //compatibility logic here
+        var lowestScore = 0;//initializing a varialb eto represent our lowestScore aka our best match - the least difference between answers
+        //we will set temp score to lowest score if its lower than the previous iterations temp score value, i think this will work!
 
+        //compatibility logic here
+        for (var i = 0; i < friendData.length; i++) {
+            console.log("--------------------------------------------------");
+            console.log("index " + i + " of friendArray: " + friendData[i]);
+            console.log(friendData[i].name);
+            console.log(friendData[i].photo);
+            console.log(friendData[i].scores);
+            if (tempScore) {
+                if (tempScore)
+            }
+            var tempScore = compare(req.body.scores, friendData[i].scores);//here we'll hold a value representing a compatibilty score for each possible match
+        }
+        //plan... each iteration through the above loop will yield a compatibility score
+        //our current friend, our submission, is located of course within req.body, so that is the one to compare against
+        //this function will compare one array against another and yield a score
+        function compare(arr1, arr2) {
+            var sum = 0;
+            for (var i = 0; i < arr1.length; i++) {
+                var diff = Math.abs(arr1[i] - arr2[i]);//calcuatles the difference between their response at each index
+                sum = sum + diff;//add that difference to sum we'll return that at the end as their compatibilty score
+            }
+            return sum;
+        }
 
 
 
